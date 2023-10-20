bb6489fa99444f32533e76918525efc5cd9e344b
Adnan Syed <141570307+sleepy-chap@users.noreply.github.com>
2023-10-20 11:49:36 +0500

    Update settings.py

--- Dev/settings.py
+++ Dev/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base'
+    'base',
+     'rest_framework'
 ]
 
 AUTH_USER_MODEL = 'base.User'
