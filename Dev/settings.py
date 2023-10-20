1bcdc40f10be5bd406b56a95782a1aa1665d937d
Adnan Syed <141570307+sleepy-chap@users.noreply.github.com>
2023-10-20 12:06:27 +0500

    Update settings.py

--- Dev/settings.py
+++ Dev/settings.py
@@ -36,8 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base',
-     'rest_framework'
+    'base'
 ]
 
 AUTH_USER_MODEL = 'base.User'
@@ -79,10 +78,12 @@ WSGI_APPLICATION = 'Dev.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+   'default': {
+        'NAME': 'devine',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': '9867947'
+    }
 }
 
 
